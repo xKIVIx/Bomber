87f4fb7227275f177aee3d57c6515f2e7fa96331
Максим <hj09@mail.ru>
2016-04-28 00:08:33 +1000

    добавлен логер

---
+++ loger/Source.cpp
@@ -0,0 +1,59 @@
+
+#define __LOG_DLL
+
+
+#include "log_error.h"
+#include <fstream>
+#include <time.h>
+#include <Windows.h>
+
+
+
+_LOG_API void LogSendMessage(int type_message,std::string block,std::string message)
+{
+	// задание пути к лог файлу
+	time_t t;
+	tm tk;
+	time(&t);
+	localtime_s(&tk, &t);
+	char * tmp;
+	tmp = new char[5];
+	_itoa_s(tk.tm_mday, tmp, 5, 10);
+	std::string name = tmp;
+	tmp = new char[5];
+	_itoa_s(tk.tm_mon + 1, tmp, 5, 10);
+	name = name + "_" + tmp;
+	tmp = new char[5];
+	_itoa_s(tk.tm_year + 1900, tmp, 5, 10);
+	name = name + "_" + tmp;
+	std::string path = "log\\" + block;
+
+	//открытие или создание лог файла
+	std::fstream file;
+	CreateDirectoryA("log", NULL);
+	CreateDirectoryA(path.c_str(), NULL);
+	path = path + "\\" + name + ".log";
+	file.open(path, std::ios::app);
+	tmp = new char[5];
+	_itoa_s(tk.tm_hour, tmp, 5, 10);
+	name = tmp;
+	tmp = new char[5];
+	_itoa_s(tk.tm_min, tmp, 5, 10);
+	name = name + ":" + tmp;
+	tmp = new char[5];
+	_itoa_s(tk.tm_sec, tmp, 5, 10);
+	name = name + ":" + tmp;
+	delete (tmp);
+
+	switch (type_message)
+	{
+	case LOG_CRITICAL_ERROR: message = "[CRITICAL_ERROR]" + name + "	" + message + "\n"; break;
+	case LOG_ERROR: message = "[ERROR]" + name + "	" + message + "\n"; break;
+	case LOG_WARNING: message = "[WARNING]" + name + "	" + message + "\n"; break;
+	case LOG_INFO: message = "[INFO] " + name + "	" + message + "\n"; break;
+	case LOG_DOING: message = "[DOING]" + name + "	" + message + "\n"; break;
+	default:message = "[UNDEF]" + name + "	" + message + "\n"; break;
+	}
+	file << message.c_str();
+	file.close();
+}
--- loger/log_error.h
+++ loger/log_error.h
@@ -1,1 +1,21 @@
+#define LOG_ERROR 1000
+#define LOG_CRITICAL_ERROR 1001
+#define LOG_WARNING 1002
+#define LOG_DOING 1003
+#define LOG_INFO 1004
 #pragma once
+//определяем типо заголовка
+#ifdef __LOG_DLL
+#define _LOG_API _declspec(dllexport)
+#else
+#define	_LOG_API _declspec(dllimport)
+#endif
+//включение/выключение логирования
+#ifdef LOG_ON
+#define LogSend(type_message, block, message) LogSendMessage(type_message,block, message)
+#else
+#define	LogSend(type_message, block, message)
+#endif
+#include <string>
+_LOG_API void LogSendMessage(int type_message, std::string block, std::string message);
+
